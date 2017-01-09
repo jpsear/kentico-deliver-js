029eff830cd0f80b557c3a46958caeb15bfe0448
James Sear <james.s@mmtdigital.co.uk>
2017-01-09 16:54:44 +0000

    Adds published content query

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+import { publishedContent, unpublishedContent } from './config/endpoints';
+require('isomorphic-fetch');
+
 class KenticoDeliverAPI {
 	constructor(projectId) {
 		if (typeof projectId !== 'string') {
 			throw new Error(`KenticoDeliverAPI must be instantiated with a Project ID`)
 		}
-		
-		this.query = {
-			values: [`/items?`]
+		this.query = { 
+			uriEndpoint: `items`,
+			values: [],
+			projectId
 		}
-
 		return this;
 	}
 
 	run() {
 		return new Promise((resolve, reject) => {
-			// Kentico fetch will go here
-			resolve(this.query)
+			this.query.queryText = _combineQueryValues(this.query.values);
+
+			this.fetchData(this.query, resolve, reject)
+				.then(result => resolve(result))
+				.catch(error => reject(error))
 		})
+	}
+
+	fetchData(query, resolve, reject) {
+		const { queryText, uriEndpoint, projectId } = this.query;
+
+		if (!query.published) {
+			// fetch unpublished data
+		}
+		else {
+			console.log(`${publishedContent}/${projectId}/${uriEndpoint}?${queryText}`);
+			return fetch(`${publishedContent}/${projectId}/${uriEndpoint}?${queryText}`)
+				.then(result => { 
+					resolve(result.json()) 
+				})
+				.catch(error => {
+					reject(error)
+				})
+		}
 	}
 
 	handler(val, fn) {
@@ -53,21 +77,25 @@ class KenticoDeliverAPI {
 	}
 }
 
+const _combineQueryValues = (values) => {
+	return values.join('');
+}
+
 const _id = (id) => {
 	if (typeof id !== 'string') {
 		throw new Error(`id must be passed as a string`)
 	}
 	if (id.indexOf(' ') >= 0) {
-		throw new Error(`id must not contain spaces`)
+		throw new Error(`id must not contain spaces`);
 	}
-	return `&system.id=${id}&`;
+	return `&system.id=${id}`;
 }
 
 const _name = (name) => {
 	if (typeof name !== 'string') {
 		throw new Error(`name must be passed as a string`)
 	}
-	return `&system.name=${encodeURIComponent(name)}&`;
+	return `&system.name=${encodeURIComponent(name)}`;
 };
 
 const _codeName = (codeName) => {
@@ -75,35 +103,35 @@ const _codeName = (codeName) => {
 		throw new Error(`codeName must be passed as a string`)
 	}
 	if (codeName.indexOf(' ') >= 0) {
-		throw new Error(`codeName must not contain spaces`)
+		throw new Error(`codeName must not contain spaces`);
 	}
-	return `&system.codename=${codeName}&`;
+	return `&system.codename=${codeName}`;
 };
 
 const _type = (type) =>  {
 	if (typeof type !== 'string') {
-		throw new Error(`type must be passed as a string`)
+		throw new Error(`type must be passed as a string`);
 	}
-	return `&system.type=${encodeURIComponent(type)}&`;
+	return `&system.type=${encodeURIComponent(type)}`;
 };
 
 const _sitemapLocation = (sitemapLocation) => {
 	if (typeof sitemapLocation !== 'string') {
 		throw new Error(`sitemapLocation must be passed as a string`)
 	}
-	return `&system.sitemap_locations[contains]=${encodeURIComponent(sitemapLocation)}&`
+	return `&system.sitemap_locations[contains]=${encodeURIComponent(sitemapLocation)}`;
 };
 
 const _lastModified = (lastModified) => {
 	if (!lastModified instanceof Date) {
-		throw new Error(`lastModified must be passed as a Date object`)
+		throw new Error(`lastModified must be passed as a Date object`);
 	}
-	return `&system.last_modified=${lastModified}&`;
+	return `&system.last_modified=${lastModified}`;
 };
 
 const _published = (published) => {
 	if (typeof published !== 'boolean') {
-		throw new Error(`published must be passed as a boolean value`)
+		throw new Error(`published must be passed as a boolean value`);
 	}
 	if (published === undefined) {
 		return true;
