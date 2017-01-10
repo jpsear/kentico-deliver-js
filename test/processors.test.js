b52c4eb9adedde22e12735389b43fa24883056be
James Sear <james.s@mmtdigital.co.uk>
2017-01-10 16:59:21 +0000

    Adds a whole host of tests for the functional methods

--- test/processors.test.js
+++ test/processors.test.js
@@ -12,9 +12,57 @@ describe('Processor tests', () => {
 		expect(_combineQueryValues(query.values)).to.equal(expected);
 	})
 
-	it('should return an id query without an operator', () => {
-		const id = '123';
+	it('should return a system.id query without an operator', () => {
 		const expected = '&system.id=123';
-		expect(_id(id)).to.equal(expected);
+		expect(_id('123')).to.equal(expected);
+	})
+
+	it('should return a system.id query with an operator', () => {
+		const expected = '&system.id[lt]=123';
+		expect(_id('123', 'lt')).to.equal(expected);
+	})
+
+	it('should return a system.type query that is encoded, without an operator', () => {
+		const expected = '&system.type=some%20type';
+		expect(_type('some type')).to.equal(expected);
+	})
+
+	it('should return a system.type query that is encoded, with an operator', () => {
+		const expected = '&system.type[contains]=some%20type';
+		expect(_type('some type', 'contains')).to.equal(expected);
+	})
+
+	it('should return a system.sitemap_locations query that is encoded, without an operator', () => {
+		const expected = '&system.sitemap_locations=About%20Us';
+		expect(_sitemapLocation('About Us')).to.equal(expected);
+	})
+
+	it('should return a system.sitemap_locations query that is encoded, with an operator', () => {
+		const expected = '&system.sitemap_locations[contains]=About%20Us';
+		expect(_sitemapLocation('About Us', 'contains')).to.equal(expected);
+	})
+
+	it('should return a system.last_modified query without an operator', () => {
+		const date = new Date();
+		const expected = `&system.last_modified=${date}`;
+		expect(_lastModified(date)).to.equal(expected);
+	})
+
+	it('should return a system.last_modified query with an operator', () => {
+		const date = new Date();
+		const expected = `&system.last_modified[lt]=${date}`;
+		expect(_lastModified(date, 'lt')).to.equal(expected);
+	})
+
+	it('should return a true published status', () => {
+		expect(_published(true)).to.equal(true);
+	})
+
+	it('should return a false published status when passing a preview API key', () => {
+		expect(_published(false, 'my-preview-api-key')).to.equal(false);
+	})
+
+	it('should throw an error when passing a false published status and not passing a preview API key', () => {
+		expect(() => _published(false)).to.throw(Error);
 	})
 })
