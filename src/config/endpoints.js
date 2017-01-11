4c6bfb17c9c903f9daa5cf1eba7d7aa436d652bd
James Sear <james.s@mmtdigital.co.uk>
2017-01-11 12:18:39 +0000

    endpoints.json —> endpoints.js

---
+++ src/config/endpoints.js
@@ -0,0 +1,10 @@
+const publishedContent = "https://deliver.kenticocloud.com";
+const unpublishedContent = "https://preview-deliver.kenticocloud.com";
+
+const endpoints = {	
+  publishedContent,
+  unpublishedContent
+}
+
+export default endpoints
+export { publishedContent, unpublishedContent }
--- test/api.test.js
+++ test/api.test.js
@@ -5,48 +5,48 @@ describe('API tests', (done) => {
   const expect = chai.expect;
 
   it('should throw an error when instantiated without a Project ID', () => {
-		expect(() => new KenticoDeliverAPI()).to.throw(Error);
+    expect(() => new KenticoDeliverAPI()).to.throw(Error);
   })
 
   it('should throw an error when instantiated with an invalid Project ID', () => {
-		expect(() => new KenticoDeliverAPI(123)).to.throw(Error);
+    expect(() => new KenticoDeliverAPI(123)).to.throw(Error);
   })
 
   it('should throw an error when Preview API Key is not a string', () => {
-		expect(() => new KenticoDeliverAPI('123', 123)).to.throw(Error);
+    expect(() => new KenticoDeliverAPI('123', 123)).to.throw(Error);
   })
 
   it('should return a query with a system.type and a less than operator ', done => {
-		const api = new KenticoDeliverAPI('123');
-		const expected = '&system.type[lt]=some_type';
+    const api = new KenticoDeliverAPI('123');
+    const expected = '&system.type[lt]=some_type';
 
-		api
-			.type('some_type', 'lessThan')
-			.queryDebug()
-			.then(result => {
-				try {
-					expect(result.queryText).to.equal(expected);
-					expect(result.published).to.equal(true);
-					done();
-				}
-				catch(error) { done(error) }
-			});
+    api
+      .type('some_type', 'lessThan')
+      .queryDebug()
+      .then(result => {
+        try {
+          expect(result.queryText).to.equal(expected);
+          expect(result.published).to.equal(true);
+          done();
+        }
+        catch(error) { done(error) }
+      });
   })
 
   it('should return a query with a system.id with no operator ', done => {
-		const api = new KenticoDeliverAPI('123');
-		const expected = '&system.id=321';
+    const api = new KenticoDeliverAPI('123');
+    const expected = '&system.id=321';
 
-		api
-			.id('321')
-			.queryDebug()
-			.then(result => {
-				try {
-					expect(result.queryText).to.equal(expected);
-					expect(result.published).to.equal(true);
-					done();
-				}
-				catch(error) { done(error) }
-			});
+    api
+      .id('321')
+      .queryDebug()
+      .then(result => {
+        try {
+          expect(result.queryText).to.equal(expected);
+          expect(result.published).to.equal(true);
+          done();
+        }
+        catch(error) { done(error) }
+      });
   })
 })
